b6973bb85bc099012841031d10af7d6bf5b71e11
fabiononato <echoaidev@gmail.com>
2023-03-24 10:30:35 -0700

    add sha to version

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+import subprocess
+
+# get current git commit sha
+commit_sha = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode()
 
 setup(
     name="echoai-dev",
-    version="0.1.0",
+    version=f"0.1.0+{commit_sha[:5]}",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
