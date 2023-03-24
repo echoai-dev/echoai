ed23b6084079e5e9765d88689e65e72344ba6f1d
fabiononato <echoaidev@gmail.com>
2023-03-24 10:37:34 -0700

    added long description to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,22 @@ import subprocess
 # get current git commit sha
 commit_sha = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="echoai-dev",
     version=f"0.1.0+{commit_sha[:5]}",
+    author="Fabio Dr.No Nonato",
+    author_email="echoaidev@gmail.com",
+    description="The command line tool to interface with Generative AI",
+    long_description=long_description,
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
     entry_points={
         "console_scripts": [
             "echoai=echoai.main:main",
