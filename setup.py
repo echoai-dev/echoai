145b48bdacab3e61f245851cf1014284d230cca6
fabiononato <echoaidev@gmail.com>
2023-03-24 17:09:17 -0700

    Improve README

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author_email="echoaidev@gmail.com",
     description="The command line tool to interface with Generative AI.",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
